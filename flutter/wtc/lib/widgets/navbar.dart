061379988d009e8b555ee8f494b83eba37bf9d09
Tanner Stephenson <tstephenson2@ewu.edu>
2024-04-03 17:17:09 -0700

    Repository catch up

---
+++ flutter/wtc/lib/widgets/navbar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class NavigationExample extends StatefulWidget {
+  const NavigationExample({Key? key}) : super(key: key);
+
+  @override
+  State<NavigationExample> createState() => _NavigationExampleState();
+}
+
+class _NavigationExampleState extends State<NavigationExample> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.amber,
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Badge(child: Icon(Icons.notifications_sharp)),
+            label: 'Notifications',
+          ),
+          NavigationDestination(
+            icon: Badge(
+              label: Text('2'),
+              child: Icon(Icons.messenger_sharp),
+            ),
+            label: 'Messages',
+          ),
+        ],
+      ),
+      body: <Widget>[
+        // Your pages here
+      ][currentPageIndex],
+    );
+  }
+}
