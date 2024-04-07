174252c11601100273624f85023e61ad1f5f5fb5
Tanner Stephenson <tstephenson2@ewu.edu>
2024-04-07 16:33:18 -0700

    Top bar integration

--- flutter/wtc/lib/widgets/topbar.dart
+++ flutter/wtc/lib/widgets/topbar.dart
@@ -10,61 +10,9 @@ class TopBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Welcome to Cheney'),
-      ),
-      drawer: () {
-        return Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              const DrawerHeader(
-                child: const Text('Menu'),
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                ),
-              ),
-              ListTile(
-                title: const Text('Home'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: const Text('Alerts'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: const Text('Jobs'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: const Text('Volunteer'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: const Text('Account'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: const Text('Create Post'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          ),
-        );
-      }(),
+    return AppBar(
+      title: const Text('Welcome to Cheney'),
+      //Drawer handles the rest
     );
   }
 }
