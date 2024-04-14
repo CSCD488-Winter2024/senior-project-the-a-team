d66cd06b0c90b6cb8da8961aefa8492951bd4fdb
Nolan Posey <nposey1@ewu.edu>
2024-04-14 14:10:34 -0700

    Added User class, refactored Post widget.

--- flutter/wtc/lib/widgets/post_widgets/post.dart
+++ flutter/wtc/lib/widgets/post_widgets/post.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_guid/flutter_guid.dart';
+import 'package:wtc/User/User.dart';
 import 'package:wtc/widgets/post_widgets/post_body_box.dart';
 import 'package:wtc/widgets/post_widgets/post_tag_box.dart';
 import 'package:wtc/widgets/post_widgets/post_title_box.dart';
@@ -9,15 +11,21 @@ class Post extends StatelessWidget {
       required this.title,
       required this.body,
       required this.tags,
-      required this.isAlert,
-      required this.isEvent})
+      required this.postId,
+      required this.header,
+      required this.user,
+      required this.interestCount,
+      required this.created})
       : super(key: key);
 
+  final Guid postId;
   final String title;
-  final String body;
+  final String header;
   final List<String> tags;
-  final bool isAlert;
-  final bool isEvent;
+  final String body;
+  final User user;
+  final int interestCount;
+  final DateTime created;
 
   @override
   Widget build(BuildContext context) {
