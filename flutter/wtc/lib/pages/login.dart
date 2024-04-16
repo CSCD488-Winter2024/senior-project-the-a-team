669dabddf946eab9aee93df1736c6a28688a6571
Daniel Palmer <114208935+dpalmer10@users.noreply.github.com>
2024-04-16 16:14:37 -0700

    fixed login screen

--- flutter/wtc/lib/pages/login.dart
+++ flutter/wtc/lib/pages/login.dart
@@ -40,76 +40,78 @@ class _LoginPageState extends State<LoginPage>{
   Widget build(BuildContext context){
     return Scaffold(
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(25.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //logo
-              SizedBox(
-                height: 250,
-                width: 250,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(40),
-                  child: const Image(image: AssetImage("images/wtcLogo.png")),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //logo
+                SizedBox(
+                  height: 250,
+                  width: 250,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(40),
+                    child: const Image(image: AssetImage("images/wtcLogo.png")),
+                  ),
                 ),
-              ),
 
-              const SizedBox(height: 25,),
+                const SizedBox(height: 25,),
 
-              // app name
-              const Text(
-                "Welcome To Cheney",
-                style: TextStyle(fontSize: 20),
-              ),
+                // app name
+                const Text(
+                  "Welcome To Cheney",
+                  style: TextStyle(fontSize: 20),
+                ),
 
-              
-              // email textfield
-              MyTextField(
-                hintText: "Email", 
-                obscureText: false, 
-                controller: emailController
-              ),
+                
+                // email textfield
+                MyTextField(
+                  hintText: "Email", 
+                  obscureText: false, 
+                  controller: emailController
+                ),
 
-              const SizedBox(height: 10,),
+                const SizedBox(height: 10,),
 
-              // password text
-              MyTextField(
-                hintText: "Password", 
-                obscureText: true, 
-                controller: passwordController
-              ),
+                // password text
+                MyTextField(
+                  hintText: "Password", 
+                  obscureText: true, 
+                  controller: passwordController
+                ),
 
-              const SizedBox(height: 10,),
+                const SizedBox(height: 10,),
 
-              //forgor password
-              GestureDetector(
-                onTap: (){
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const ForgotPasswordPage())
-                  );
-                },
-                child: const Text("Forgot Password"),
-              ),
+                //forgor password
+                GestureDetector(
+                  onTap: (){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const ForgotPasswordPage())
+                    );
+                  },
+                  child: const Text("Forgot Password"),
+                ),
 
-              const SizedBox(height: 25,),
+                const SizedBox(height: 25,),
 
-              //sign in
-              MyButton(
-                text: 'Login', 
-                onTap: login
-              ),
+                //sign in
+                MyButton(
+                  text: 'Login', 
+                  onTap: login
+                ),
 
-              const SizedBox(height: 10,),
+                const SizedBox(height: 10,),
 
-              // register
-              GestureDetector(
-                onTap: widget.onTap,
-                child: const Text("Register Here"),
-              ),
-            ]
-          ),
+                // register
+                GestureDetector(
+                  onTap: widget.onTap,
+                  child: const Text("Register Here"),
+                ),
+              ]
+            ),
+          )
         )
       )
     );
