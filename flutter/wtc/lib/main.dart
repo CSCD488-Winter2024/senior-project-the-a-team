d89bdd29752af6f5f45eeab6758b1dff043ef65b
Tanner Stephenson <tstephenson2@ewu.edu>
2024-04-21 15:39:29 -0700

    Auto stash before merge of "create_post_db" and "origin/development"

--- flutter/wtc/lib/main.dart
+++ flutter/wtc/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:wtc/auth/auth.dart';
+import 'package:wtc/auth/login_or_register.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:wtc/firebase_options.dart';
+// Import the firebase_core package
 //import 'app.dart';
 
 /*
@@ -9,8 +11,7 @@ import 'package:wtc/firebase_options.dart';
   -- home is set to our nav bar since this will be housing our page
 
 */
-
-void main() async { 
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const WTCApp());
@@ -22,10 +23,9 @@ class WTCApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true),
-      home: const AuthPage() //App entry point
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(useMaterial3: true),
+        home: const LoginOrRegister() //App entry point
+        );
   }
 }
-
