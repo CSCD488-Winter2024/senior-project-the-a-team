ec0368cfc2b871e1bb619982ef4d46e95976f365
DennysGuy <mattmatriciano@gmail.com>
2024-04-23 08:39:32 -0700

    removed a few blank lines

--- flutter/wtc/lib/pages/alerts.dart
+++ flutter/wtc/lib/pages/alerts.dart
@@ -1,79 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_guid/flutter_guid.dart';
-import 'package:wtc/User/user.dart';
 import 'package:wtc/widgets/alerts_list.dart';
-import 'package:wtc/widgets/post_widgets/post.dart';
-
-User user =
-    User(userId: Guid.newGuid, username: "TestUser", email: "TestUser@wtc.org");
-
-Post post1 = Post(
-  postId: Guid.newGuid,
-  header: "The header",
-  user: user,
-  interestCount: 0,
-  created: DateTime.now(),
-  title: "Test1",
-  body:
-      "Lets see what happens when I write a huge amount of text. I mean like really write an absurd amount of text just to test this simple little feature. Because I need to be sure that this will work as it is intended too.",
-  tags: ["tag1", "tag2", "tag3", "alert"],
-  userEmail: 'tester@gmail.com',
-);
-Post post2 = Post(
-  postId: Guid.newGuid,
-  header: "The header",
-  user: user,
-  interestCount: 0,
-  created: DateTime.now(),
-  title: "Test2",
-  body: "This is a test",
-  tags: ["tag1", "tag2"],
-  userEmail: 'tester@gmail.com',
-);
-Post post3 = Post(
-  postId: Guid.newGuid,
-  header: "The header",
-  user: user,
-  interestCount: 0,
-  created: DateTime.now(),
-  title: "Test3",
-  body: "This is a test",
-  tags: ["tag1", "tag2", "tag7", "alert"],
-  userEmail: 'tester@gmail.com',
-);
-Post post4 = Post(
-  postId: Guid.newGuid,
-  header: "The header",
-  user: user,
-  interestCount: 0,
-  created: DateTime.now(),
-  title: "Test4",
-  body: "This is a test",
-  tags: ["tag1", "tag2"],
-  userEmail: 'tester@gmail.com',
-);
-Post post5 = Post(
-  postId: Guid.newGuid,
-  header: "The header",
-  user: user,
-  interestCount: 0,
-  created: DateTime.now(),
-  title: "Test5",
-  body: "This is a test",
-  tags: ["tag1"],
-  userEmail: 'tester@gmail.com',
-);
-
-List<Post> posts = <Post>[
-  post1,
-  post2,
-  post3,
-  post4,
-  post5,
-];
 
 class AlertsPage extends StatefulWidget {
-  const AlertsPage({Key? key}) : super(key: key);
+  const AlertsPage({super.key});
 
   @override
   State<AlertsPage> createState() => _AlertsPage();
@@ -87,9 +16,7 @@ class _AlertsPage extends State<AlertsPage> {
       shadowColor: Colors.transparent,
       margin: const EdgeInsets.all(8.0),
       child: SizedBox.expand(
-          child: AlertsList(
-        postList: posts,
-      )),
+          child: AlertsList()),
     );
   }
 }
