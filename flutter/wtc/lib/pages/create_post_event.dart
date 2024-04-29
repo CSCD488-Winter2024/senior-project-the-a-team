662f832fb38c9f08cd4ffc9503c265260580f57d
Tanner Stephenson <tstephenson2@ewu.edu>
2024-04-28 17:11:50 -0700

    Header controller restrictions
    Co-authored-by: Nolan Posey <nposey1@ewu.edu>

--- flutter/wtc/lib/pages/create_post_event.dart
+++ flutter/wtc/lib/pages/create_post_event.dart
@@ -102,10 +102,12 @@ class _CreatePostEventPageState extends State<CreatePostEventPage> {
       return; // Exit the function if validation fails
     }
 
-    if (_titleController.text.length > 60) {
+    if (_titleController.text.length > 60 ||
+        _headerController.text.length > 60) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
-            content: Text('Title can have no more than 60 characters.')),
+            content: Text(
+                'Title or quick description can have no more than 60 characters.')),
       );
       return; // Exit the function if validation fails
     }
