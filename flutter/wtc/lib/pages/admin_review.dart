5c7682c504cd85246df18bdbe5d0e67f8e94518d
Tanner Stephenson <tstephenson2@ewu.edu>
2024-04-30 13:25:34 -0700

    Post list for review populating

--- flutter/wtc/lib/pages/admin_review.dart
+++ flutter/wtc/lib/pages/admin_review.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:wtc/widgets/post_widgets/post_list.dart';
+import 'package:wtc/widgets/post_widgets/post_list_review.dart';
 
 class AdminReviewPage extends StatefulWidget {
   const AdminReviewPage({super.key});
 
   @override
-  State<AdminReviewPage> createState() => _HomePage();
+  State<AdminReviewPage> createState() => _AdminReviewPage();
 }
 
-class _HomePage extends State<AdminReviewPage> {
+class _AdminReviewPage extends State<AdminReviewPage> {
   @override
   Widget build(BuildContext context) {
-    //final ThemeData theme = Theme.of(context);
     return Card(
       shadowColor: Colors.transparent,
       margin: const EdgeInsets.all(8.0),
       child: SizedBox.expand(
-        child: PostList(),
+        child: PostListReview(),
       ),
     );
   }
