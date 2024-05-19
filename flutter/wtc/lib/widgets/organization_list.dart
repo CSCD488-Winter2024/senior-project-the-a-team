4b1559c5114f4f2794f6849ead99b73ebd0b03a0
DennysGuy <mattmatriciano@gmail.com>
2024-05-19 15:47:50 -0700

    starting organization post list

---
+++ flutter/wtc/lib/widgets/organization_list.dart
@@ -0,0 +1,62 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_guid/flutter_guid.dart';
+import 'package:wtc/widgets/post_widgets/post.dart';
+
+class OrganizationList extends StatelessWidget {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  OrganizationList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: _firestore
+            .collection('_users')
+            .orderBy('timestamp', descending: true)
+            .where('type', isEqualTo: 'Alert')
+            .snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text("Error: ${snapshot.error}"));
+          }
+
+          return ListView.separated(
+            padding: const EdgeInsets.all(8),
+            itemCount: snapshot.data?.docs.length ?? 0,
+            itemBuilder: (BuildContext context, int index) {
+              var document = snapshot.data?.docs[index];
+              String dateCreated = document?['createdAt'] as String;
+    
+              var tempTags = document?['tags'] as List<dynamic>;
+              List<String> postTags = [];
+
+              for (int i = 0; i < tempTags.length; i++) {
+                postTags.add(tempTags[i]);
+              }
+              return Post(
+                  postId: Guid(document?['postID'] as String),
+                  header: document?['header'] as String,
+                  userEmail: document?['user'] as String,
+                  interestCount: document?['interestCount'] as int,
+                  created: DateTime(
+                      int.parse(dateCreated.split("-")[0]),
+                      int.parse(dateCreated.split("-")[1]),
+                      int.parse(dateCreated.split("-")[2])),
+                  title: document?['title'] as String,
+                  tags: postTags,
+                  body: document?['body'] as String,
+                );
+              }
+            ,
+            separatorBuilder: (BuildContext context, int index) =>
+                const Divider(),
+          );
+        });
+  }
+}
