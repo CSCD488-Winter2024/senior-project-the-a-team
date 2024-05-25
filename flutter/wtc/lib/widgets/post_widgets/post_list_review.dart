8724c720c06bf4b77f4bfa51546a9d17991470d4
Tanner Stephenson <tstephenson2@ewu.edu>
2024-05-25 14:01:58 -0700

    Post list review
    Post list review no longer contains admin functions.

--- flutter/wtc/lib/widgets/post_widgets/post_list_review.dart
+++ flutter/wtc/lib/widgets/post_widgets/post_list_review.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_guid/flutter_guid.dart';
 import 'package:wtc/widgets/event_widgets/event.dart';
 import 'package:wtc/widgets/job_post/job_post.dart';
-import 'package:wtc/widgets/post_widgets/post.dart';
 import 'package:wtc/widgets/post_widgets/post_review.dart';
 
 class PostListReview extends StatelessWidget {
