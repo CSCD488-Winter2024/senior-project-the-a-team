ab2799c21139d215f4ec172d32bae1ed430408e7
Nolan Posey <nposey1@ewu.edu>
2024-05-26 18:20:57 -0700

    Text now has ellipses if goes off screen on the search users page.

--- flutter/wtc/lib/pages/search_user.dart
+++ flutter/wtc/lib/pages/search_user.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:wtc/widgets/user_widgets/search_user_info.dart';
 
@@ -14,6 +15,7 @@ class _SearchUserPageState extends State<SearchUserPage> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   List<QueryDocumentSnapshot> allUsers = [];
   List<QueryDocumentSnapshot> filteredUsers = [];
+  User? currentUser = FirebaseAuth.instance.currentUser;
 
   @override
   void initState() {
@@ -22,7 +24,10 @@ class _SearchUserPageState extends State<SearchUserPage> {
   }
 
   Future<void> getAllUsers() async {
-    var querySnapshot = await _firestore.collection('users').get();
+    var querySnapshot = await _firestore
+        .collection('users')
+        .where('email', isNotEqualTo: currentUser?.email)
+        .get();
     setState(() {
       allUsers = querySnapshot.docs;
       filteredUsers = querySnapshot.docs;
