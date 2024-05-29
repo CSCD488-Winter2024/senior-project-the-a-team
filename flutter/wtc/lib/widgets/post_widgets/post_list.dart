26c0f50ef0f979e97570e51556896edbcd32aad5
Nolan Posey <nposey1@ewu.edu>
2024-05-29 02:27:05 -0700

    Added LiquidPullToRefresh widget to homepage. Currently doesn't do anything but neat animation.

--- flutter/wtc/lib/widgets/post_widgets/post_list.dart
+++ flutter/wtc/lib/widgets/post_widgets/post_list.dart
@@ -7,6 +7,7 @@ import 'package:wtc/widgets/job_post/job_post.dart';
 import 'package:wtc/widgets/post_widgets/post.dart';
 import 'package:wtc/widgets/post_widgets/postlist_no_posts.dart';
 import 'package:wtc/widgets/post_widgets/postlist_no_tags.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 
 class PostList extends StatefulWidget {
   const PostList({super.key});
@@ -33,6 +34,8 @@ class _PostListState extends State<PostList> {
     List<String> userTags = userTagsDynamic.cast<String>();
     return userTags;
   }
+
+  Future<void> _testRefresh() async {}
 
   void _refresh() {
     setState(() {
@@ -76,110 +79,115 @@ class _PostListState extends State<PostList> {
                     userTags: userTags, refreshPage: _refresh);
               }
 
-              return ListView.separated(
-                padding: const EdgeInsets.all(8),
-                itemCount: snapshot.data?.docs.length ?? 0,
-                itemBuilder: (BuildContext context, int index) {
-                  var document = snapshot.data?.docs[index];
-                  String type = document?['type'] as String;
-                  String dateCreated = document?['createdAt'] as String;
+              return LiquidPullToRefresh(
+                color: const Color(0xFF469AB8),
+                backgroundColor: const Color(0xffd4bc93),
+                onRefresh: _testRefresh,
+                child: ListView.separated(
+                  padding: const EdgeInsets.all(8),
+                  itemCount: snapshot.data?.docs.length ?? 0,
+                  itemBuilder: (BuildContext context, int index) {
+                    var document = snapshot.data?.docs[index];
+                    String type = document?['type'] as String;
+                    String dateCreated = document?['createdAt'] as String;
 
-                  // Use if-else block to return the correct type of Post for the proper formatting
-                  if (type == "Event") {
-                    var tempTags = document?['tags'] as List<dynamic>;
-                    List<String> postTags = tempTags.cast<String>();
+                    // Use if-else block to return the correct type of Post for the proper formatting
+                    if (type == "Event") {
+                      var tempTags = document?['tags'] as List<dynamic>;
+                      List<String> postTags = tempTags.cast<String>();
 
-                    String dateOfEvent = document?['eventDate'] as String;
-                    String timeOfEvent = document?['eventTime'] as String;
-                    var time = timeOfEvent.split(' ');
-                    int hour = 0;
-                    if (time[1] == "PM") {
-                      if (time[0].split(":")[0] == "12") {
-                        hour = 12;
+                      String dateOfEvent = document?['eventDate'] as String;
+                      String timeOfEvent = document?['eventTime'] as String;
+                      var time = timeOfEvent.split(' ');
+                      int hour = 0;
+                      if (time[1] == "PM") {
+                        if (time[0].split(":")[0] == "12") {
+                          hour = 12;
+                        } else {
+                          hour = int.parse(time[0].split(":")[0]) + 12;
+                        }
                       } else {
-                        hour = int.parse(time[0].split(":")[0]) + 12;
+                        hour = int.parse(time[0].split(":")[0]);
                       }
-                    } else {
-                      hour = int.parse(time[0].split(":")[0]);
-                    }
-                    int minute = int.parse(time[0].split(":")[1]);
+                      int minute = int.parse(time[0].split(":")[1]);
 
-                    return Event(
-                      title: document?['title'] as String,
-                      body: document?['body'] as String,
-                      tags: postTags,
-                      header: document?['header'] as String,
-                      interestCount: document?['interestCount'] as int,
-                      created: DateTime(
-                          int.parse(dateCreated.split("-")[0]),
-                          int.parse(dateCreated.split("-")[1]),
-                          int.parse(dateCreated.split("-")[2])),
-                      postId: Guid(document?['postID'] as String),
-                      userEmail: document?['user'] as String,
-                      date: DateTime(
-                          int.parse(dateOfEvent.split("-")[0]),
-                          int.parse(dateOfEvent.split("-")[1]),
-                          int.parse(dateOfEvent.split("-")[2])),
-                      time: TimeOfDay(hour: hour, minute: minute),
-                      location: document?['address'] as String,
-                      attendingCount: document?['attendingCount'] as int,
-                      maybeCount: document?['maybeCount'] as int,
-                    );
-                  } else if (type == "Job") {
-                    var wage = document?['wage'];
-                    if (wage is int) {
-                      wage = wage.toDouble();
+                      return Event(
+                        title: document?['title'] as String,
+                        body: document?['body'] as String,
+                        tags: postTags,
+                        header: document?['header'] as String,
+                        interestCount: document?['interestCount'] as int,
+                        created: DateTime(
+                            int.parse(dateCreated.split("-")[0]),
+                            int.parse(dateCreated.split("-")[1]),
+                            int.parse(dateCreated.split("-")[2])),
+                        postId: Guid(document?['postID'] as String),
+                        userEmail: document?['user'] as String,
+                        date: DateTime(
+                            int.parse(dateOfEvent.split("-")[0]),
+                            int.parse(dateOfEvent.split("-")[1]),
+                            int.parse(dateOfEvent.split("-")[2])),
+                        time: TimeOfDay(hour: hour, minute: minute),
+                        location: document?['address'] as String,
+                        attendingCount: document?['attendingCount'] as int,
+                        maybeCount: document?['maybeCount'] as int,
+                      );
+                    } else if (type == "Job") {
+                      var wage = document?['wage'];
+                      if (wage is int) {
+                        wage = wage.toDouble();
+                      }
+                      return JobPost(
+                        title: document?['title'] as String,
+                        body: document?['body'] as String,
+                        tags: const ["Job"],
+                        header: document?['header'] as String,
+                        userEmail: document?['user'] as String,
+                        interestCount: document?['interestCount'] as int,
+                        created: DateTime(
+                            int.parse(dateCreated.split("-")[0]),
+                            int.parse(dateCreated.split("-")[1]),
+                            int.parse(dateCreated.split("-")[2])),
+                        postId: Guid(document?['postID'] as String),
+                        wage: wage,
+                        wageType: document?['wageType'] as String,
+                      );
+                    } else if (type == "Volunteer") {
+                      return JobPost(
+                        title: document?['title'] as String,
+                        body: document?['body'] as String,
+                        tags: const ["Volunteer"],
+                        header: document?['header'] as String,
+                        userEmail: document?['user'] as String,
+                        interestCount: document?['interestCount'] as int,
+                        created: DateTime(
+                            int.parse(dateCreated.split("-")[0]),
+                            int.parse(dateCreated.split("-")[1]),
+                            int.parse(dateCreated.split("-")[2])),
+                        postId: Guid(document?['postID'] as String),
+                        volunteer: true,
+                      );
+                    } else {
+                      var tempTags = document?['tags'] as List<dynamic>;
+                      List<String> postTags = tempTags.cast<String>();
+                      return Post(
+                        postId: Guid(document?['postID'] as String),
+                        header: document?['header'] as String,
+                        userEmail: document?['user'] as String,
+                        interestCount: document?['interestCount'] as int,
+                        created: DateTime(
+                            int.parse(dateCreated.split("-")[0]),
+                            int.parse(dateCreated.split("-")[1]),
+                            int.parse(dateCreated.split("-")[2])),
+                        title: document?['title'] as String,
+                        tags: postTags,
+                        body: document?['body'] as String,
+                      );
                     }
-                    return JobPost(
-                      title: document?['title'] as String,
-                      body: document?['body'] as String,
-                      tags: const ["Job"],
-                      header: document?['header'] as String,
-                      userEmail: document?['user'] as String,
-                      interestCount: document?['interestCount'] as int,
-                      created: DateTime(
-                          int.parse(dateCreated.split("-")[0]),
-                          int.parse(dateCreated.split("-")[1]),
-                          int.parse(dateCreated.split("-")[2])),
-                      postId: Guid(document?['postID'] as String),
-                      wage: wage,
-                      wageType: document?['wageType'] as String,
-                    );
-                  } else if (type == "Volunteer") {
-                    return JobPost(
-                      title: document?['title'] as String,
-                      body: document?['body'] as String,
-                      tags: const ["Volunteer"],
-                      header: document?['header'] as String,
-                      userEmail: document?['user'] as String,
-                      interestCount: document?['interestCount'] as int,
-                      created: DateTime(
-                          int.parse(dateCreated.split("-")[0]),
-                          int.parse(dateCreated.split("-")[1]),
-                          int.parse(dateCreated.split("-")[2])),
-                      postId: Guid(document?['postID'] as String),
-                      volunteer: true,
-                    );
-                  } else {
-                    var tempTags = document?['tags'] as List<dynamic>;
-                    List<String> postTags = tempTags.cast<String>();
-                    return Post(
-                      postId: Guid(document?['postID'] as String),
-                      header: document?['header'] as String,
-                      userEmail: document?['user'] as String,
-                      interestCount: document?['interestCount'] as int,
-                      created: DateTime(
-                          int.parse(dateCreated.split("-")[0]),
-                          int.parse(dateCreated.split("-")[1]),
-                          int.parse(dateCreated.split("-")[2])),
-                      title: document?['title'] as String,
-                      tags: postTags,
-                      body: document?['body'] as String,
-                    );
-                  }
-                },
-                separatorBuilder: (BuildContext context, int index) =>
-                    const Divider(),
+                  },
+                  separatorBuilder: (BuildContext context, int index) =>
+                      const Divider(),
+                ),
               );
             },
           );
