3a9f118ff49b89cbf51bcd83295f9e275e3e0012
Nolan Posey <nposey1@ewu.edu>
2024-05-28 17:36:11 -0700

    Message added to home page if there is no posts for selected tags. Button added for easy access of edit tags page.

---
+++ flutter/wtc/lib/widgets/post_widgets/postlist_no_posts.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:wtc/accountPages/edit_tags.dart';
+
+class PostListNoPosts extends StatelessWidget {
+  const PostListNoPosts(
+      {super.key, required this.userTags, required this.refreshPage});
+
+  final List<String> userTags;
+  final VoidCallback refreshPage;
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> origTags = [];
+    origTags.addAll(userTags);
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Text(
+            "There are currently no posts marked with your selected tags.",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        const SizedBox(
+          height: 20,
+        ),
+        const Text(
+          "Try selecting some more tags from the select tags menu. You can get there from the account page by clicking the \"Settings\" button and then clicking \"Edit Tags\" or by clicking the button below.",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        OutlinedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        EditTags(tags: userTags, origTags: origTags)),
+              ).whenComplete(() => refreshPage());
+            },
+            child: const Text("Edit Your Tags Here!"))
+      ],
+    );
+  }
+}
