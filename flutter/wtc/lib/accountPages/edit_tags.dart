7d739ac86e5d1c2f2e9fdc5e5800ce93caa79589
Nolan Posey <nposey1@ewu.edu>
2024-06-03 19:21:10 -0700

    Fixed merge conflict bug

--- flutter/wtc/lib/accountPages/edit_tags.dart
+++ flutter/wtc/lib/accountPages/edit_tags.dart
@@ -62,56 +62,47 @@ class _EditTagsState extends State<EditTags> {
     }
   }
 
-  Future<bool?> _showBackDialog() async{
+  Future<bool?> _showBackDialog() async {
     return await showDialog(
-      context: context, 
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Are you sure?'),
-          content: const Text('You have unsaved changes.'),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(false), 
-              child: const Text('Nevermind'),
-            ),
-
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(true), 
-              child: const Text('Leave'),
-            )
-          ],
-        );
-      }
-    );
-  } 
-
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Are you sure?'),
+            content: const Text('You have unsaved changes.'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: const Text('Nevermind'),
+              ),
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(true),
+                child: const Text('Leave'),
+              )
+            ],
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () async{
-            if(listEquals(widget.tags, widget.origTags)){
-              Navigator.of(context).pop();
-            }
-            else{
-              bool shouldPop = await _showBackDialog() ?? false;
-              if(shouldPop && context.mounted){
-                setState(() {
-                  widget.tags.clear();
-                  widget.tags.addAll(widget.origTags);
-                });
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () async {
+              if (listEquals(widget.tags, widget.origTags)) {
                 Navigator.of(context).pop();
+              } else {
+                bool shouldPop = await _showBackDialog() ?? false;
+                if (shouldPop && context.mounted) {
+                  setState(() {
+                    widget.tags.clear();
+                    widget.tags.addAll(widget.origTags);
+                  });
+                  Navigator.of(context).pop();
+                }
               }
-            }
-          },
-          icon: const Icon(Icons.arrow_back),
-        ),
-        title: const Text(
-          "Edit Tags",
-          style: TextStyle(
-            color: Colors.white
+            },
+            icon: const Icon(Icons.arrow_back),
           ),
           title: const Text(
             "Edit Tags",
@@ -121,192 +112,131 @@ class _EditTagsState extends State<EditTags> {
           backgroundColor: const Color(0xFF469AB8),
           iconTheme: const IconThemeData(color: Colors.white),
         ),
-        centerTitle: true,
-        backgroundColor: const Color(0xFF469AB8),
-        iconTheme: const IconThemeData(
-          color: Colors.white
-        ),
-      ),
-
-      body: Center(
-        //child: SingleChildScrollView(
+        body: Center(
+          //child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-
               PopScope(
                 canPop: false,
-                onPopInvoked: (bool didPop) async{
-                  if(didPop){
+                onPopInvoked: (bool didPop) async {
+                  if (didPop) {
                     return;
                   }
-                  final bool changes = !listEquals(widget.tags, widget.origTags);
-                  if(changes){
+                  final bool changes =
+                      !listEquals(widget.tags, widget.origTags);
+                  if (changes) {
                     final bool shouldPop = await _showBackDialog() ?? false;
-                    if(context.mounted && shouldPop){
+                    if (context.mounted && shouldPop) {
                       setState(() {
                         widget.tags.clear();
                         widget.tags.addAll(widget.origTags);
                       });
                       Navigator.of(context).pop();
                     }
-                  }
-                  else{
+                  } else {
                     Navigator.of(context).pop();
                   }
                 },
-                child: const SizedBox(), 
+                child: const SizedBox(),
               ),
-              
               const Align(
                 //alignment: Alignment.centerLeft,
                 child: Padding(
                   padding: EdgeInsets.all(8.0),
                   child: Text(
                     "Select Tags",
-                    style: TextStyle(
-                      fontSize: 21,
-                      fontWeight: FontWeight.bold
-                    ),
+                    style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
                   ),
-                ),  
+                ),
               ),
-
               Padding(
                 padding: const EdgeInsets.all(10.0),
                 child: Align(
-                  alignment: Alignment.center,
-                  child: Wrap(
-                    spacing: 1.0,
-                    alignment: WrapAlignment.center,
-                    runSpacing: 4.0,
-                    children: items.map(
-                      (e) => Padding(
-                        padding: const EdgeInsets.all(5),
-                        child: FilterChip(
-                          showCheckmark: false,
-                          label: Text(e), 
-                          selected: widget.tags.contains(e),
-                          onSelected: (bool value) {
-                            if(widget.tags.contains(e)){
-                              widget.tags.remove(e);
-                            }else{
-                              widget.tags.add(e);
-                            }
-                            setState(() {});
-                          }
-                        )
-                      ),
-                    ).toList(),         
-                  )    
-                ),
+                    alignment: Alignment.center,
+                    child: Wrap(
+                      spacing: 1.0,
+                      alignment: WrapAlignment.center,
+                      runSpacing: 4.0,
+                      children: items
+                          .map(
+                            (e) => Padding(
+                                padding: const EdgeInsets.all(5),
+                                child: FilterChip(
+                                    showCheckmark: false,
+                                    label: Text(e),
+                                    selected: widget.tags.contains(e),
+                                    onSelected: (bool value) {
+                                      if (widget.tags.contains(e)) {
+                                        widget.tags.remove(e);
+                                      } else {
+                                        widget.tags.add(e);
+                                      }
+                                      setState(() {});
+                                    })),
+                          )
+                          .toList(),
+                    )),
               ),
-
               const Spacer(),
-
               ElevatedButton(
-                onPressed: () async{
-                  if(listEquals(widget.tags, widget.origTags)){
-                    Navigator.of(context).pop();
-                  }
-                  else{
-                    await showDialog(
-                      context: context, 
-                      builder: (context) {
-                        return AlertDialog(
-                          title: const Text('Confirm Tags?'),
-                          actions: [
-                            TextButton(
-                              onPressed: () => Navigator.of(context).pop(),
-                              child: const Text('No'),
-                            ),
+                  onPressed: () async {
+                    if (listEquals(widget.tags, widget.origTags)) {
+                      Navigator.of(context).pop();
+                    } else {
+                      await showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text('Confirm Tags?'),
+                              actions: [
+                                TextButton(
+                                  onPressed: () => Navigator.of(context).pop(),
+                                  child: const Text('No'),
+                                ),
+                                TextButton(
+                                  onPressed: () async {
+                                    showDialog(
+                                        barrierDismissible: false,
+                                        context: context,
+                                        builder: (context) {
+                                          return const AlertDialog(
+                                            title: Text('Updating Tags...'),
+                                            content: LinearProgressIndicator(),
+                                          );
+                                        });
+                                    await updateTags(widget.tags);
+                                    await setTags(widget.tags, widget.origTags);
 
-                            TextButton(
-                              onPressed: ()async{
-                                showDialog(
-                                  barrierDismissible: false,
-                                  context: context, 
-                                  builder: (context) {
-                                    return const AlertDialog(
-                                      title: Text('Updating Tags...'),
-                                      content: LinearProgressIndicator(),
-                                    );
-                                  }
-                                );
-                                await updateTags(widget.tags);
-                                await setTags(widget.tags, widget.origTags);
+                                    setState(() {
+                                      widget.origTags.clear();
+                                      widget.origTags.addAll(widget.tags);
+                                      GlobalUserInfo.setData(
+                                          'tags', widget.tags);
+                                    });
+                                    ScaffoldMessenger.of(context)
+                                        .showSnackBar(const SnackBar(
+                                      content:
+                                          Text('Tags Updated Successfully'),
+                                      duration: Duration(seconds: 3),
+                                    ));
 
-                                setState(() {
-                                  widget.origTags.clear();
-                                  widget.origTags.addAll(widget.tags);
-                                });
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                    content: Text('Tags Updated Successfully'),
-                                    duration: Duration(seconds: 3),
-                                  )
-                                );
-
-                                Navigator.of(context).pop();
-                                Navigator.of(context).pop();
-                                Navigator.of(context).pop();
-                              },
-                              child: const Text('Yes'),
-                            )
-                            .toList(),
-                      )),
-                ),
-                const SizedBox(
-                  height: 150,
-                ),
-                ElevatedButton(
-                    onPressed: () async {
-                      if (listEquals(widget.tags, widget.origTags)) {
-                        Navigator.of(context).pop();
-                      } else {
-                        await showDialog(
-                            context: context,
-                            builder: (context) {
-                              return AlertDialog(
-                                title: const Text('Confirm Tags?'),
-                                actions: [
-                                  TextButton(
-                                    onPressed: () =>
-                                        Navigator.of(context).pop(),
-                                    child: const Text('No'),
-                                  ),
-                                  TextButton(
-                                    onPressed: () async {
-                                      await updateTags(widget.tags);
-                                      await setTags(
-                                          widget.tags, widget.origTags);
-
-                                      setState(() {
-                                        widget.origTags.clear();
-                                        widget.origTags.addAll(widget.tags);
-                                        GlobalUserInfo.setData(
-                                            'tags', widget.tags);
-                                      });
-
-                                      Navigator.of(context).pop();
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: const Text('Yes'),
-                                  )
-                                ],
-                              );
-                            });
-                      }
-                    );
-                  }
-                }, 
-                child: const Text("Confirm")
-              ),
+                                    Navigator.of(context).pop();
+                                    Navigator.of(context).pop();
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text('Yes'),
+                                )
+                              ],
+                            );
+                          });
+                    }
+                  },
+                  child: const Text("Confirm")),
               const SizedBox(height: 40)
             ],
           ),
-        //),
-      )
-    );
+          //),
+        ));
   }
 }
