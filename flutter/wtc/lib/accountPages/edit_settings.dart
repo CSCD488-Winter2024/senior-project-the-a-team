7da4809ebe3552986b3ff593cf90bf23eee1b6b6
Daniel Palmer <114208935+dpalmer10@users.noreply.github.com>
2024-06-10 14:07:05 -0700

    Update edit_settings.dart

--- flutter/wtc/lib/accountPages/edit_settings.dart
+++ flutter/wtc/lib/accountPages/edit_settings.dart
@@ -1,16 +1,19 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:showcaseview/showcaseview.dart';
 import 'package:wtc/accountPages/account_upgrade.dart';
 import 'package:wtc/accountPages/edit_business.dart';
 import 'package:wtc/accountPages/edit_profile.dart';
 import 'package:wtc/accountPages/edit_tags.dart';
 import 'package:wtc/auth/delete_account.dart';
+import 'package:wtc/auth/link_accounts.dart';
 import 'package:wtc/components/settings_button.dart';
 
 class EditSettings extends StatefulWidget {
   const EditSettings({
-    super.key,
+    Key? key,
     required this.tier,
     required this.email,
     required this.tags,
@@ -20,7 +23,7 @@ class EditSettings extends StatefulWidget {
     required this.profilePic,
     required this.uid,
     required this.isPending,
-  });
+  }) : super(key: key);
 
   final String tier;
   final String email;
@@ -41,6 +44,7 @@ class _EditSettingsState extends State<EditSettings> {
 
   final provider =
       FirebaseAuth.instance.currentUser?.providerData.first;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   bool tour = true;
 
