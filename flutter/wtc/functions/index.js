0faf155e1c5dd46bd0953c042d4c5a5aacc4b013
Nolan Posey <nposey1@ewu.edu>
2024-06-11 19:11:25 -0700

    Refactored the firebase functions

--- flutter/wtc/functions/index.js
+++ flutter/wtc/functions/index.js
@@ -7,8 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-// const {onRequest} = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
 
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
